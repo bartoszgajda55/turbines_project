22f94fc847dffc67fab32a6a3755a6ea1c68ac73
Bartosz Gajda <bartoszgajda55@outlook.com>
2025-03-14 20:48:18 +0100

    Add the base working Python project.

--- turbines/src/turbines/main.py
+++ turbines/src/turbines/main.py
@@ -12,7 +12,7 @@ def get_spark() -> SparkSession:
     try:
         from databricks.connect import DatabricksSession
 
-        return DatabricksSession.builder.getOrCreate()
+        return DatabricksSession.builder.clusterId(clusterId="0314-194013-7dfx96xt").getOrCreate()
     except ImportError:
         return SparkSession.builder.getOrCreate()
 
