4b84320679bc1737e0835b1337755523d92bedeb
Bartosz Gajda <bartoszgajda55@outlook.com>
2025-03-16 13:04:47 +0100

    Change the env loading.

--- turbines/tests/conftest.py
+++ turbines/tests/conftest.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-from pathlib import Path
 from dotenv import load_dotenv
 from databricks.connect import DatabricksSession
 from turbines.logger import logger
@@ -11,18 +10,12 @@ def spark():
     Create a Spark session using Databricks Connect.
     Cluster ID is read from .env file.
     """
-    dotenv_file = (
-        Path(__file__).parent.parent / ".env"
-    )  # Assumes .env file is in the root of the Python project (not whole project)
-    if dotenv_file:
-        logger.info(f"Loading environment variables from {dotenv_file}")
-        load_dotenv(dotenv_file)
-    else:
-        logger.warning("No .env file found. Some code may fail due to lack of Env Vars.")
+    logger.info("Loading environment variables from .env file")
+    load_dotenv()
     
     cluster_id = os.getenv("CLUSTER_ID")
     if not cluster_id:
-        raise ValueError("CLUSTER_ID not found in .env file")
+        raise ValueError("CLUSTER_ID not found in Env Variables. Set it in .env file (if locally) or in the GH Variables (if run in Actions).")
     
     spark = DatabricksSession.builder.clusterId(clusterId=cluster_id).getOrCreate()
     yield spark
